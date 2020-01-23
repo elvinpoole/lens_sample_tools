885e200c0b802a15570a016d458fde93b64d6dc4
Jack Elvin-Poole <jack.elvinpoole@gmail.com>
2020-01-23 00:07:53 -0500

    updated fit template script to work on maglim

--- nz/clustering_z/template_fit/fit_template.py
+++ nz/clustering_z/template_fit/fit_template.py
@@ -6,180 +6,223 @@ import pylab as plt
 
 import autograd 
 import scipy.interpolate as interp
-import misc
 import scipy.optimize
 import scipy.stats
 import h5py
+import os
 
-outdir = misc.dir('./nov19_plots/')
-plotdir = misc.dir(outdir + './plots/')
+outdir = './output/'
+plotdir = './plots/'
 
-#rmdir = '/Users/jackelvinpoole/DES/cats/y3/redmagic/combined_sample_6.4.22/'
-rm_nz_table = np.loadtxt( 'nz_combined_overlap.txt', unpack=True )
+#do you want to propagate the gamma uncertainty into the n(z) covariance?
+#'N' to ignore gamma uncertainty, 'J' to use the jacobian, 'R' for random draws
+#apply_gamma_error = 'N'
+#apply_gamma_error = 'J'
+apply_gamma_error = 'R'
 
-xcorr_file_list = [	'../eboss_xcorr_redshifts_mar19/y3_6.4.22_hidens_bin0_eboss5_mar19/BNz_Schmidt___biasr_AC_R_D___jackknife_pairs_100.h5',
-					'../eboss_xcorr_redshifts_mar19/y3_6.4.22_hidens_bin1_eboss5_mar19/BNz_Schmidt___biasr_AC_R_D___jackknife_pairs_100.h5',
-					'../eboss_xcorr_redshifts_mar19/y3_6.4.22_hidens_bin2_eboss5_mar19/BNz_Schmidt___biasr_AC_R_D___jackknife_pairs_100.h5',
-					'../eboss_xcorr_redshifts_mar19/y3_6.4.22_hilum_bin3_eboss5_mar19/BNz_Schmidt___biasr_AC_R_D___jackknife_pairs_100.h5',
-					'../eboss_xcorr_redshifts_mar19/y3_6.4.22_hilum_bin4_eboss5_mar19/BNz_Schmidt___biasr_AC_R_D___jackknife_pairs_100.h5', ]
+ndraws = 100000 #used if apply_gamma_error = R
 
-gamma_correct_list = [
-	#(gamma, gamma_var),
-	(0.,1.5),
-	(0.,1.5),
-	(0.,1.5),
-	(0.,2.),
-	(0.,2.),
+
+samples = [
+	{
+		'label':'rm_0.5.1_wide_0.9binning_{apply_gamma_error}'.format(apply_gamma_error=apply_gamma_error),
+		'wz_data_dir':'../data/y3_redmagic_clusteringz_nov20_2019/',
+		'nz_overlap_file':'output/nz_combined_overlap_rm_0.5.1_wide_0.9binning.txt',
+		'gammaarray':[-0.1,0.7,-0.1,2.4,-0.3],
+		'gammaunc':[0.5,0.5,1.25,1.25,2.0],
+		'nbins':5,
+	}, 
+	{
+		'label':'maglim_v22_{apply_gamma_error}'.format(apply_gamma_error=apply_gamma_error),
+		'wz_data_dir':'../data/y3_maglim_clusteringz_nov20_2019/',
+		'nz_overlap_file':'output/nz_combined_overlap_maglim_v22.txt',
+		'gammaarray':[-1.1,-3.1,0.7,3.7,1.9,-2.4],
+		'gammaunc':[1.5,2.0,1.5,2.5,3.0,2.5],
+		'nbins':6,
+	},
 ]
 
-apply_gamma_error = False
-use_jacobian = True
-
-fitted_nz_table = []
-fitted_nz_table.append(rm_nz_table[0])
-coeff_mean_list = []
-coeff_width_list = []
-coeff_mean_width_list = []
-delta_z_y1_method_list = []
-for ibin in xrange(5):
-	print 'bin', ibin+1
-
-	#load the redmagic n(z) as a 'theory' prediction
-	z_theory = rm_nz_table[0]
-	nz_theory = rm_nz_table[ibin+1]
-
-	#load the xcorr n(z) as a data prediction
-	xcorr_file =h5py.File(xcorr_file_list[ibin])
-	z_edges_data = np.transpose(xcorr_file['z_edges/block0_values'].value)[0]
-	z_data = np.transpose(xcorr_file['z/block0_values'].value)[0]
-	nz_data = np.transpose(xcorr_file['results/block0_values'].value)[0]
-	cov = np.transpose(xcorr_file['cov/block0_values'].value)
-	err = np.sqrt(np.diagonal(cov))
-	dz_data = z_edges_data[1]-z_edges_data[0]
-
-	##### apply bias correction #####
-	def apply_gamma(nz_and_gamma):
-		gammacorrection = (1. + z_data)**nz_and_gamma[-1]
-		norm_gamma = sum(nz_and_gamma[:-1])/sum(nz_and_gamma[:-1]*gammacorrection)
-		return nz_and_gamma[:-1] * gammacorrection * norm_gamma
-
-	gamma = gamma_correct_list[ibin][0]
-	gamma_var = gamma_correct_list[ibin][1]
-	nz_and_gamma = np.append(nz_data, gamma)
-	nz_data = apply_gamma(nz_and_gamma)
-
-	#propagate the gamma variance into the n(z) covariance with a jacobian
-	if apply_gamma_error==True and use_jacobian == True:
-		print 'propagating gamma error in n(z) using the jacobian'
-		j = autograd.jacobian( apply_gamma )
-		covmat = np.zeros((len(nz_data)+1,len(nz_data)+1))
-		covmat[:-1,:-1] = cov
-		covmat[-1,-1] = gamma_var 
-		cov_corrected = np.matrix(j(nz_and_gamma))*np.matrix(covmat)*np.matrix(j(nz_and_gamma)).T
-		cov_corrected = np.array(cov_corrected)
-		cov = cov_corrected
-		err = np.sqrt(np.diagonal(cov))
-		np.savetxt(outdir + 'cov_corrected_bin{0}_jacobian_method.txt'.format(ibin+1), cov_corrected)
-
-	#propagate the gamma variance into the n(z) covariance with random draws
-	elif apply_gamma_error==True and use_jacobian == False :
-		print 'propagating gamma error in n(z) using random draws from the data'
-		ndraws = 100000
-		nz_draws = np.random.multivariate_normal(nz_data, cov, ndraws)
-		gamma_draws = np.random.multivariate_normal([0], [[gamma_var]], ndraws)
-		nz_and_gamma_draws = np.hstack((nz_draws,gamma_draws)) 
-		nz_corrected_draws =  np.array([apply_gamma(nz_and_gamma_draw) for nz_and_gamma_draw in nz_and_gamma_draws])
-		cov_corrected = np.cov(nz_corrected_draws, rowvar=False)
-		cov = cov_corrected
-		err = np.sqrt(np.diagonal(cov))
-		np.savetxt(outdir + 'cov_corrected_bin{0}_random_draws_method.txt'.format(ibin+1), cov_corrected)
+def calc_chi2( y, err, yfit ):
+	if err.shape == (len(y),len(y)): #use full covariance
+		inv_cov = np.linalg.inv( np.matrix(err) )
+		chi2 = 0
+		for i in xrange(len(y)):
+			for j in xrange(len(y)):
+				chi2 = chi2 + (y[i]-yfit[i])*inv_cov[i,j]*(y[j]-yfit[j])
+		return chi2
+	elif err.shape == (len(y),): #use sqrt(diagonal)
+		return sum(((y-yfit)**2.)/(err**2.))
 	else:
-		print 'not applying the gamma error'
+		raise IOError('error in err or cov_mat input shape')
 
-	select_range = (z_theory > z_edges_data.min())*(z_theory < z_edges_data.max())
-	norm = dz_data
-	nz_theory = norm*nz_theory
+for sample in samples:
+	nz_table = np.loadtxt( sample['nz_overlap_file'], unpack=True )
+	xcorr_file_list = np.sort([sample['wz_data_dir']+f for f in os.listdir(sample['wz_data_dir']) if '.h5' in f])
 
-	def shift_1(z_eval,deltaz):
-		"""
-		n(z) = n_fid(z + deltaz)
-		"""
-		return interp.interp1d(z_theory+deltaz, nz_theory, fill_value=0.,bounds_error=False)(z_eval)
+	#check you have the correct number of bins
+	assert len(sample['gammaarray']) == sample['nbins']
+	assert len(sample['gammaunc']) == sample['nbins']
+	assert len(nz_table) == sample['nbins'] + 1
+	assert len(xcorr_file_list) == sample['nbins']
 
-	def stretch_1(z_eval,stretch):
-		"""
-		n(z) = n_fid(stretch*(z - zmean) + zmean)
-		"""
-		zmean = np.average(z_theory,weights=nz_theory)
-		return interp.interp1d(stretch*(z_theory-zmean)+zmean, nz_theory,fill_value=0.,bounds_error=False)(z_eval)/stretch
+	fitted_nz_table = []
+	fitted_nz_table.append(nz_table[0])
+	coeff_mean_list = []
+	coeff_width_list = []
+	coeff_mean_width_list = []
+	delta_z_y1_method_list = []
+	for ibin in xrange(sample['nbins']):
+		print 'bin', ibin+1
 
-	def stretch_shift_1(z_eval,stretch,deltaz):
-		"""
-		n(z) = n_fid(stretch*(z - zmean) + zmean + deltaz) I think
-		"""
-		zmean = np.average(z_theory,weights=nz_theory)
-		return interp.interp1d(stretch*(z_theory-zmean)+zmean+deltaz, nz_theory, fill_value=0.,bounds_error=False)(z_eval)/stretch
+		#load the photo-z n(z) as a 'theory' prediction
+		z_theory = nz_table[0]
+		nz_theory = nz_table[ibin+1]
+		dz_theory = z_theory[1]-z_theory[0]
+
+		#load the xcorr n(z) as a data prediction
+		xcorr_file =h5py.File(xcorr_file_list[ibin])
+		z_edges_data = np.transpose(xcorr_file['z_edges/block0_values'].value)[0]
+		z_data = np.transpose(xcorr_file['z/block0_values'].value)[0]
+		nz_data = np.transpose(xcorr_file['results/block0_values'].value)[0]
+		cov = np.transpose(xcorr_file['cov/block0_values'].value)
+		err = np.sqrt(np.diagonal(cov))
+		dz_data = z_edges_data[1]-z_edges_data[0]
+
+		##### apply bias correction #####
+		def apply_gamma(nz_and_gamma):
+			gammacorrection = (1. + z_data)**nz_and_gamma[-1]
+			norm_gamma = sum(nz_and_gamma[:-1])/sum(nz_and_gamma[:-1]*gammacorrection)
+			return nz_and_gamma[:-1] * gammacorrection * norm_gamma
+
+		#gamma = gamma_correct_list[ibin][0]
+		#gamma_var = gamma_correct_list[ibin][1]
+		gamma = sample['gammaarray'][ibin]
+		gamma_var = sample['gammaunc'][ibin]
+		nz_and_gamma = np.append(nz_data, gamma)
+		nz_data = apply_gamma(nz_and_gamma)
+
+		if apply_gamma_error == 'N':
+			print('not applying the gamma error')
+
+		#propagate the gamma variance into the n(z) covariance with a jacobian
+		elif apply_gamma_error == 'J':
+			print('propagating gamma error in n(z) using the jacobian')
+			j = autograd.jacobian( apply_gamma )
+			covmat = np.zeros((len(nz_data)+1,len(nz_data)+1))
+			covmat[:-1,:-1] = cov
+			covmat[-1,-1] = gamma_var 
+			cov_corrected = np.matrix(j(nz_and_gamma))*np.matrix(covmat)*np.matrix(j(nz_and_gamma)).T
+			cov_corrected = np.array(cov_corrected)
+			cov = cov_corrected
+			err = np.sqrt(np.diagonal(cov))
+			np.savetxt(outdir + '{label}_cov_corrected_bin{ibin}_jacobian_method.txt'.format(ibin=ibin+1, label=sample['label']), cov_corrected)
+
+		#propagate the gamma variance into the n(z) covariance with random draws
+		elif apply_gamma_error == 'R':
+			print('propagating gamma error in n(z) using random draws from the data')
+			nz_draws = np.random.multivariate_normal(nz_data, cov, ndraws)
+			gamma_draws = np.random.multivariate_normal([0], [[gamma_var]], ndraws)
+			nz_and_gamma_draws = np.hstack((nz_draws,gamma_draws)) 
+			nz_corrected_draws =  np.array([apply_gamma(nz_and_gamma_draw) for nz_and_gamma_draw in nz_and_gamma_draws])
+			cov_corrected = np.cov(nz_corrected_draws, rowvar=False)
+			cov = cov_corrected
+			err = np.sqrt(np.diagonal(cov))
+			np.savetxt(outdir + '{label}_cov_corrected_bin{ibin}_random_draws_method.txt'.format(ibin=ibin+1, label=sample['label']), cov_corrected)
+		else:
+			raise IOError("apply_gamma_error should be 'N', 'J' or 'R' ")
+
+		select_range = (z_theory > z_edges_data.min())*(z_theory < z_edges_data.max())
+		
+		#normalise the photoz nz to match the clustering-z 
+		#norm = dz_data
+		norm = (sum(nz_data)*dz_data)/(sum(nz_theory)*dz_theory)
+		nz_theory = norm*nz_theory
+
+		def shift_1(z_eval,deltaz):
+			"""
+			n(z) = n_fid(z + deltaz)
+			"""
+			return interp.interp1d(z_theory+deltaz, nz_theory, fill_value=0.,bounds_error=False)(z_eval)
+
+		def stretch_1(z_eval,stretch):
+			"""
+			n(z) = n_fid(stretch*(z - zmean) + zmean)
+			"""
+			zmean = np.average(z_theory,weights=nz_theory)
+			return interp.interp1d(stretch*(z_theory-zmean)+zmean, nz_theory,fill_value=0.,bounds_error=False)(z_eval)/stretch
+
+		def stretch_shift_1(z_eval,stretch,deltaz):
+			"""
+			n(z) = n_fid(stretch*(z - zmean) + zmean + deltaz) I think
+			"""
+			zmean = np.average(z_theory,weights=nz_theory)
+			return interp.interp1d(stretch*(z_theory-zmean)+zmean+deltaz, nz_theory, fill_value=0.,bounds_error=False)(z_eval)/stretch
 
 
-	delta_z_y1_method = np.trapz(z_data*nz_data,x=z_data)/np.trapz(nz_data,x=z_data)-np.trapz(z_theory*nz_theory,x=z_theory)/np.trapz(nz_theory,x=z_theory)
-	delta_z_y1_method_list.append(delta_z_y1_method)
+		#fun the template fits (and a y1 style difference in means for comparison)
+		delta_z_y1_method = np.trapz(z_data*nz_data,x=z_data)/np.trapz(nz_data,x=z_data)-np.trapz(z_theory*nz_theory,x=z_theory)/np.trapz(nz_theory,x=z_theory)
+		delta_z_y1_method_list.append(delta_z_y1_method)
 
-	coeff, coeff_cov = scipy.optimize.curve_fit(shift_1, z_data, nz_data, p0=[0.], sigma=cov,  absolute_sigma=True)
-	coeff_mean_list.append(coeff)
-	np.savetxt(outdir + 'nz_mean_cov_bin{0}.txt'.format(ibin+1), coeff_cov )
+		coeff, coeff_cov = scipy.optimize.curve_fit(shift_1, z_data, nz_data, p0=[0.], sigma=cov,  absolute_sigma=True)
+		coeff_mean_list.append(coeff)
+		np.savetxt(outdir + '{label}_nz_mean_cov_bin{ibin}_.txt'.format(ibin=ibin+1, label=sample['label']), coeff_cov )
 
-	coeff, coeff_cov = scipy.optimize.curve_fit(stretch_1, z_data, nz_data, p0=[1.], sigma=cov,  absolute_sigma=True)
-	coeff_width_list.append(coeff)
-	np.savetxt(outdir + 'nz_width_cov_bin{0}.txt'.format(ibin+1), coeff_cov )
+		coeff, coeff_cov = scipy.optimize.curve_fit(stretch_1, z_data, nz_data, p0=[1.], sigma=cov,  absolute_sigma=True)
+		coeff_width_list.append(coeff)
+		np.savetxt(outdir + '{label}_nz_width_cov_bin{ibin}.txt'.format(ibin=ibin+1, label=sample['label']), coeff_cov )
 
-	coeff, coeff_cov = scipy.optimize.curve_fit(stretch_shift_1, z_data, nz_data, p0=[1.,0.], sigma=cov,  absolute_sigma=True)
-	coeff_mean_width_list.append(coeff)
-	np.savetxt(outdir + 'nz_width_mean_cov_bin{0}.txt'.format(ibin+1), coeff_cov )
+		coeff, coeff_cov = scipy.optimize.curve_fit(stretch_shift_1, z_data, nz_data, p0=[1.,0.], sigma=cov,  absolute_sigma=True)
+		coeff_mean_width_list.append(coeff)
+		np.savetxt(outdir + '{label}_nz_width_mean_cov_bin{ibin}.txt'.format(ibin=ibin+1, label=sample['label']), coeff_cov )
 
-	ngrid = 100
-	delta_array = np.linspace(coeff[1]-3.*np.sqrt(coeff_cov[1,1]),coeff[1]+3.*np.sqrt(coeff_cov[1,1]),ngrid)
-	stretch_array = np.linspace(coeff[0]-3.*np.sqrt(coeff_cov[0,0]),coeff[0]+3.*np.sqrt(coeff_cov[0,0]),ngrid)
+		ngrid = 100
+		delta_array = np.linspace(coeff[1]-3.*np.sqrt(coeff_cov[1,1]),coeff[1]+3.*np.sqrt(coeff_cov[1,1]),ngrid)
+		stretch_array = np.linspace(coeff[0]-3.*np.sqrt(coeff_cov[0,0]),coeff[0]+3.*np.sqrt(coeff_cov[0,0]),ngrid)
 
-	X, Y = np.meshgrid(stretch_array, delta_array)
-	from scipy.stats import multivariate_normal
-	gauss = multivariate_normal(coeff, coeff_cov)
-	Z = gauss.pdf(np.transpose([X.flatten(),Y.flatten()]))
-	Z = Z.reshape(100,100)
+		########### start plot: 2d contours of the stretch+shift fit ############
+		X, Y = np.meshgrid(stretch_array, delta_array)
+		from scipy.stats import multivariate_normal
+		gauss = multivariate_normal(coeff, coeff_cov)
+		Z = gauss.pdf(np.transpose([X.flatten(),Y.flatten()]))
+		Z = Z.reshape(100,100)
+		plt.figure()
+		plt.axhline(0.0, color='k',ls='--')
+		plt.axvline(1.0, color='k',ls='--')
+		frac_levels = [0.9545,0.6827]
+		Zsort = np.sort(Z.flatten())
+		levels = [Zsort[np.abs(np.cumsum(Zsort)-(1-frac_level)*np.sum(Z)) == np.abs(np.cumsum(Zsort)-(1-frac_level)*np.sum(Z)).min()][0] for frac_level in frac_levels]
+		plt.contour(X,Y,Z,levels=levels,colors='r',label='bin {0}'.format(ibin+1))
+		plt.title('bin {0}'.format(ibin+1))
+		plt.xlabel('stretch')
+		plt.ylabel(r'$\Delta z$')
+		plt.legend()
+		plt.tight_layout()
+		plt.savefig(plotdir + '{label}_2d_contours_deltaz_stretch_bin{ibin}.png'.format(ibin=ibin+1, label=sample['label']))
+		plt.close()
+		########### end plot ############
 
-	plt.figure()
-	plt.axhline(0.0, color='k',ls='--')
-	plt.axvline(1.0, color='k',ls='--')
-	frac_levels = [0.9545,0.6827]
-	Zsort = np.sort(Z.flatten())
-	levels = [Zsort[np.abs(np.cumsum(Zsort)-(1-frac_level)*np.sum(Z)) == np.abs(np.cumsum(Zsort)-(1-frac_level)*np.sum(Z)).min()][0] for frac_level in frac_levels]
-	plt.contour(X,Y,Z,levels=levels,colors='r',label='bin {0}'.format(ibin+1))
-	plt.title('bin {0}'.format(ibin+1))
-	plt.xlabel('stretch')
-	plt.ylabel(r'$\Delta z$')
-	plt.legend()
-	plt.tight_layout()
-	plt.savefig(plotdir + '2d_contours_deltaz_stretch_bin{0}.png'.format(ibin+1))
-	plt.close()
-
-	redmagic_chi2 = misc.calc_chi2(nz_data, cov, interp.interp1d(z_theory, nz_theory)(z_data))
-	fitted_chi2   = misc.calc_chi2(nz_data, cov, interp.interp1d(z_theory, stretch_shift_1(z_theory,coeff[0],coeff[1]))(z_data))
-	plt.plot(z_theory[select_range], nz_theory[select_range], 'b-', label='redmagic, chi2={0}/{1}'.format(np.round(redmagic_chi2,decimals=1), len(nz_data)))
-	plt.plot(z_theory[select_range], stretch_shift_1(z_theory[select_range],coeff[0],coeff[1]), 'r-', label='fit to xcorr ' + r'$\Delta z, \ s$' + ' chi2={0}/{1}'.format(np.round(fitted_chi2,decimals=1),len(nz_data)))
-	plt.errorbar(z_data, nz_data, err, fmt='.', label='xcorr')
-	plt.xlabel('z')
-	plt.legend()
-	plt.savefig(plotdir + 'nz_bin{0}.png'.format(ibin+1))
-	plt.close()
+		########### start plot: plot best fit n(z) ############
+		photoz_chi2 = calc_chi2(nz_data, cov, interp.interp1d(z_theory, nz_theory)(z_data))
+		fitted_chi2   = calc_chi2(nz_data, cov, interp.interp1d(z_theory, stretch_shift_1(z_theory,coeff[0],coeff[1]))(z_data))
+		plt.plot(z_theory[select_range], nz_theory[select_range], 'b-', label='photoz, chi2={0}/{1}'.format(np.round(photoz_chi2,decimals=1), len(nz_data)))
+		plt.plot(z_theory[select_range], stretch_shift_1(z_theory[select_range],coeff[0],coeff[1]), 'r-', label='fit to xcorr ' + r'$\Delta z, \ s$' + ' chi2={0}/{1}'.format(np.round(fitted_chi2,decimals=1),len(nz_data)))
+		plt.errorbar(z_data, nz_data, err, fmt='.', label='xcorr')
+		plt.xlabel('z')
+		plt.legend()
+		plt.savefig(plotdir + '{label}_nz_bin{ibin}.png'.format(ibin=ibin+1, label=sample['label']))
+		plt.close()
+		########### end plot ############
 
 
-	fitted_nz_table.append(stretch_shift_1(z_theory,coeff[0],coeff[1]))
+		fitted_nz_table.append(stretch_shift_1(z_theory,coeff[0],coeff[1]))
 
-np.savetxt(outdir + 'nz_mean_delta_z_y1_method.txt', delta_z_y1_method_list , header='deltaz')
-np.savetxt(outdir + 'nz_mean_coeff.txt', coeff_mean_list, header='deltaz')
-np.savetxt(outdir + 'nz_width_coeff.txt', coeff_width_list, header='stretch')
-np.savetxt(outdir + 'nz_width_mean_coeff.txt', coeff_mean_width_list, header='stretch\tdeltaz')
+	#save the results
+	np.savetxt(outdir + '{label}_nz_mean_delta_z_y1_method.txt'.format(label=sample['label']), delta_z_y1_method_list , header='deltaz')
+	np.savetxt(outdir + '{label}_nz_mean_coeff.txt'.format(label=sample['label']), coeff_mean_list, header='deltaz')
+	np.savetxt(outdir + '{label}_nz_width_coeff.txt'.format(label=sample['label']), coeff_width_list, header='stretch')
+	np.savetxt(outdir + '{label}_nz_width_mean_coeff.txt'.format(label=sample['label']), coeff_mean_width_list, header='stretch\tdeltaz')
 
-np.savetxt(outdir + 'nz_combined_fitted_mean_width.txt',  np.transpose(fitted_nz_table) )
+	np.savetxt(outdir + '{label}_nz_combined_fitted_mean_width.txt'.format(label=sample['label']),  np.transpose(fitted_nz_table) )
 
 
 
